7c7a6f9f1781ea019fffcc6a9152aaa149b6d15f
atikur-rabbi <msawon94@gmail.com>
2020-12-18 03:18:08 +0600

    tmp

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ db.defaults({ users: [
 
 
 app.use(express.static(__dirname + '/public'));
-
+app.use(express.static(__dirname + '/tmp'));
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
