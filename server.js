0a57920bcfba19be5a0ab12d6ad76b828129b161
atikur-rabbi <msawon94@gmail.com>
2020-12-18 03:43:08 +0600

    ss

--- server.js
+++ server.js
@@ -33,48 +33,48 @@ app.get("/users", function (request, response) {
   response.send(dbUsers); // sends dbUsers back to the page
 });
 
-// // creates a new entry in the users collection with the submitted values
-// app.post("/users", function (request, response) {
-//   db.get('users')
-//     .push({ firstName: request.query.fName, lastName: request.query.lName })
-//     .write()
-//   console.log("New user inserted in the database");
-//   response.sendStatus(200);
-// });
+// creates a new entry in the users collection with the submitted values
+app.post("/users", function (request, response) {
+  db.get('users')
+    .push({ firstName: request.query.fName, lastName: request.query.lName })
+    .write()
+  console.log("New user inserted in the database");
+  response.sendStatus(200);
+});
 
-// // removes entries from users and populates it with default users
-// app.get("/reset", function (request, response) {
-//   // removes all entries from the collection
-//   db.get('users')
-//   .remove()
-//   .write()
-//   console.log("Database cleared");
+// removes entries from users and populates it with default users
+app.get("/reset", function (request, response) {
+  // removes all entries from the collection
+  db.get('users')
+  .remove()
+  .write()
+  console.log("Database cleared");
   
-//   // default users inserted in the database
-//   var users= [
-//       {"firstName":"John", "lastName":"Hancock"},
-//       {"firstName":"Liz",  "lastName":"Smith"},
-//       {"firstName":"Ahmed","lastName":"Khan"}
-//   ];
+  // default users inserted in the database
+  var users= [
+      {"firstName":"John", "lastName":"Hancock"},
+      {"firstName":"Liz",  "lastName":"Smith"},
+      {"firstName":"Ahmed","lastName":"Khan"}
+  ];
   
-//   users.forEach(function(user){
-//     db.get('users')
-//       .push({ firstName: user.firstName, lastName: user.lastName })
-//       .write()
-//   });
-//   console.log("Default users added");
-//   response.redirect("/");
-// });
+  users.forEach(function(user){
+    db.get('users')
+      .push({ firstName: user.firstName, lastName: user.lastName })
+      .write()
+  });
+  console.log("Default users added");
+  response.redirect("/");
+});
 
-// // removes all entries from the collection
-// app.get("/clear", function (request, response) {
-//   // removes all entries from the collection
-//   db.get('users')
-//   .remove()
-//   .write()
-//   console.log("Database cleared");
-//   response.redirect("/");
-// });
+// removes all entries from the collection
+app.get("/clear", function (request, response) {
+  // removes all entries from the collection
+  db.get('users')
+  .remove()
+  .write()
+  console.log("Database cleared");
+  response.redirect("/");
+});
 
 
 module.exports = app
